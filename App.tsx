7a49e97dc177514f3d40bde9f948e5d7bbf2eabe
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-02-19 20:50:22 +0100

    auth state change from App Stack

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { enableScreens } from 'react-native-screens';
+import { Observer } from 'mobx-react';
 
 import { AppStack } from './src/app';
+import { StoresProvider, useStores } from './src/stores';
+import { initServices, ServicesProvider, useServices } from './src/services';
 
 enableScreens();
+initServices();
 
 export default () => {
+  const { G } = useStores();
+
   useEffect(() => { start() }, []);
 
   const start = async () => { }
 
   return (
-    <>
-      <StatusBar style={'dark'} />
-      <AppStack authed />
-    </>
+    <StoresProvider><ServicesProvider>
+      <Observer>
+        {() => (
+          <>
+            <StatusBar style={'dark'} />
+            <AppStack authed={G.isAuthed} />
+          </>
+        )}
+      </Observer>
+    </ServicesProvider></StoresProvider>
   );
 }
