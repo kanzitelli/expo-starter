2411b5fd4fe09b8c0f31c5dcc87cb06d5745439b
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-02-19 23:25:43 +0100

    App waiting for stores to be persisted

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { enableScreens } from 'react-native-screens';
 import { Observer } from 'mobx-react';
@@ -6,16 +6,27 @@ import { Observer } from 'mobx-react';
 import { AppStack } from './src/app';
 import { StoresProvider, useStores } from './src/stores';
 import { initServices, ServicesProvider } from './src/services';
+import { reaction } from 'mobx';
 
 enableScreens();
 initServices();
 
 export default () => {
   const { G } = useStores();
+  const [ready, setReady] = useState(false);
 
   useEffect(() => { start() }, []);
 
-  const start = async () => { }
+  const start = async () => {
+    if (G.isSynced) startApp();
+    reaction(() => G.isSynced === true, startApp);
+  }
+
+  const startApp = () => {
+    setReady(true);
+  }
+
+  if (!ready) return null;
 
   return (
     <StoresProvider><ServicesProvider>
