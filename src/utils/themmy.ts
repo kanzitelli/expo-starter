641ea5174640a1ede2acb9ab80e2f93b7e7a388a
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-03-07 14:52:44 +0300

    small themmy utils

---
+++ src/utils/themmy.ts
@@ -0,0 +1,32 @@
+import { Theme } from '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    colors: ThemeColors;
+  }
+}
+
+const presets: Presets = {
+  light: {
+    text: '#1e272e',
+    background: 'rgb(244,244,244)',
+    primary: '#0fbcf9',
+    secondary: '#469c57',
+    accent: '#fed330',
+    muted: '#f6f6f6f',
+  },
+  dark: {
+    text: 'rgb(244,244,244)',
+    background: '#1e272e',
+    primary: '#0fbcf9',
+    secondary: '#469c57',
+    accent: '#fed330',
+    muted: '#f6f6f6f',
+  },
+}
+
+export const getTheme = (mode: ThemeMode): Theme => {
+  return {
+    colors: presets[mode],
+  }
+}
--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -36,8 +36,17 @@ interface IService {
   init: () => Promise<void>;
 }
 
-// STORES
-type Theme = 'light' | 'dark'
+// THEMMY
+type ThemeColors = {
+  text: string;       // Body foreground color
+  background: string; // Body background color
+  primary: string;    // Primary brand color for links, buttons, etc.
+  secondary: string;  // A secondary brand color for alternative styling
+  accent: string;     // A contrast color for emphasizing UI
+  muted: string;      // A faint color for backgrounds, borders, and accents that do not require high contrast with the background color
+}
+type Presets = { [key: string]: ThemeColors }
+type ThemeMode = 'light' | 'dark'
 
 // API
 // Auth Service
