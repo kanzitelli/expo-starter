bc9f254f070d2e6d2dfaf7bf4a364ea1db5614ac
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-03-10 03:46:03 +0300

    example screen & navigator localization

--- src/screens/navigator-example.tsx
+++ src/screens/navigator-example.tsx
@@ -3,23 +3,25 @@ import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 // import { createStackNavigator } from '@react-navigation/stack';
 
 import { navBarStyle } from '../utils/help';
+import { useServices } from '../services';
 
 import MainScreen from './main/main';
 // import AuthScreen from './auth/auth';
 
 const ExampleNavigator: React.FC<NavigatorProps> = ({
-  theme,
 }) => {
+  const { t } = useServices();
+
   const Stack = createNativeStackNavigator();
   // const Stack = createStackNavigator();
 
   return (
-    <Stack.Navigator screenOptions={navBarStyle(theme)}>
+    <Stack.Navigator screenOptions={navBarStyle()}>
       <Stack.Screen
         name={'Main'}
         component={MainScreen}
         options={{
-          title: 'Main',
+          title: t.do('example.title'),
         }}
       />
       {/* <Stack.Screen
