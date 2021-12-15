0974f949a17657117bfd7ff1e3a6d7ab34c04760
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-12-15 17:59:55 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+    {
+      ...env,
+      babel: {dangerouslyAddModulePathsToTranspile: ['rn-bounceable', '@kanzitelli/if-component']},
+    },
+    argv,
+  );
+
+  return config;
+};
