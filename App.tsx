f66e699e688c0f3eada089777e720edebba3e395
Batr Kanzitelli <kanzitdinov@gmail.com>
2022-01-08 22:40:43 +0100

    [Android] GestureHandlerRootView added

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import 'expo-dev-client';
 import React, {useCallback, useEffect, useState} from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import {LogBox} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 import {AppNavigator} from './src/app';
 import {configureDesignSystem} from './src/utils/designSystem';
@@ -29,8 +30,10 @@ export default (): JSX.Element => {
   }, [startApp]);
 
   return (
-    <StoresProvider>
-      <StoresProvider>{ready ? <AppNavigator /> : null}</StoresProvider>
-    </StoresProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <StoresProvider>
+        <StoresProvider>{ready ? <AppNavigator /> : null}</StoresProvider>
+      </StoresProvider>
+    </GestureHandlerRootView>
   );
 };
