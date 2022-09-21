e60522b3d014b72f578e974671f73033d853a2fd
Batyr Kanzitdinov <dev@batyr.io>
2022-09-22 01:10:29 +0200

    Update types.ts

--- src/services/navigation/types.ts
+++ src/services/navigation/types.ts
@@ -5,22 +5,16 @@ import {NativeStackNavigationOptions} from '@react-navigation/native-stack';
 import {ModalName, ScreenName, TabName} from '../../screens';
 
 type BaseScreenInfo<Name extends string, OptionsReturn = NativeStackNavigationOptions> = {
-  // name: Name;
   component: React.FC<any>;
   options: (props: {route: RouteProp<ParamListBase, Name>; navigation: any}) => OptionsReturn;
-  // component: React.FC<NativeStackScreenProps<ScreenProps, Screen>>; // idk why it doesn't work
 };
 
 export type ScreenInfo = BaseScreenInfo<ScreenName>;
 export type ScreensInfo = Record<ScreenName, ScreenInfo>;
+export type ScreensInfoPartial = Partial<ScreensInfo>;
 
 export type ModalInfo = BaseScreenInfo<ModalName>;
 export type ModalsInfo = Record<ModalName, ModalInfo>;
 
 export type TabScreenInfo = BaseScreenInfo<TabName, BottomTabNavigationOptions>;
 export type TabsInfo = Record<TabName, TabScreenInfo>;
-
-export type GenRootParams = {
-  tabs: TabsInfo;
-  modals: ModalsInfo;
-};
