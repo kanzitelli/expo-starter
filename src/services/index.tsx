642afad9a1099a1bc8368a36db62e6125a34bae7
Batyr Kanzitdinov <dev@batyr.io>
2022-10-01 20:37:58 +0200

    Services class + navio service added

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 
 import {OnStartService} from './onStart';
-import {NavService} from './navigation';
 import {TranslateService} from './translate';
 import {ApiService} from './api';
+import {getNavio} from '../screens';
 
-export const services = {
-  t: new TranslateService(), // should be first
-  nav: new NavService(),
-  onStart: new OnStartService(),
-  api: new ApiService(),
-};
-type ContextServices = typeof services;
+class Services {
+  t = new TranslateService();
+  onStart = new OnStartService();
+  api = new ApiService();
 
-const servicesContext = React.createContext<ContextServices>(services);
+  // -- adding navio as a service
+  get navio() {
+    return getNavio();
+  }
+}
+export const services = new Services();
+
+const servicesContext = React.createContext<Services>(services);
 export const ServicesProvider = ({children}: any) => (
   <servicesContext.Provider value={services}>{children}</servicesContext.Provider>
 );
-
-export const useServices = (): ContextServices => React.useContext(servicesContext);
+export const useServices = (): Services => React.useContext(servicesContext);
 
 export const initServices = async (): PVoid => {
   for (const key in services) {
     if (Object.prototype.hasOwnProperty.call(services, key)) {
-      const s = (services as Services)[key];
+      const s = (services as any)[key] as IService;
 
       if (s.init) {
         await s.init();
